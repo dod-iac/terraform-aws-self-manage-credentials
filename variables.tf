a7fc30bf63b833596663437ccec160bec12cbbf6
pjdufour-dds <patrick.dufour@dds.mil>
2021-03-25 07:45:26 -0400

    merge in MFA policy

--- variables.tf
+++ variables.tf
@@ -1,13 +1,37 @@
+variable "description" {
+  type        = string
+  description = "The description of the AWS IAM policy."
+  default     = "Allows an IAM user to manage their own credentials"
+}
+
 variable "iam_groups" {
-  description = "List of IAM groups to allow access to managing their own crednetials."
+  description = "List of IAM groups to allow access to managing their own credentials."
   type        = list(string)
   default     = []
 }
 
 variable "iam_users" {
-  description = "List of IAM users to allow access to managing their own crednetials."
+  description = "List of IAM users to allow access to managing their own credentials."
   type        = list(string)
   default     = []
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the AWS IAM policy."
+  default     = "self-manage-credentials"
+}
+
+variable "allow_account_aliases" {
+  type        = bool
+  description = "Allow users to list the account aliases."
+  default     = true
+}
+
+variable "allow_account_summary" {
+  type        = bool
+  description = "Allow users to get the account summary."
+  default     = true
 }
 
 variable "allow_access_keys" {
@@ -22,6 +46,12 @@ variable "allow_git_credentials" {
   default     = true
 }
 
+variable "allow_mfa_device" {
+  type        = bool
+  description = "Allow users to manage their own MFA device."
+  default     = true
+}
+
 variable "allow_signing_certificates" {
   type        = bool
   description = "Allow users to manage their own signing certificates."
@@ -32,4 +62,16 @@ variable "allow_ssh_keys" {
   type        = bool
   description = "Allow users to manage their own SSH keys."
   default     = true
+}
+
+variable "require_mfa" {
+  type        = bool
+  description = "Requires users to login with MFA when managing their own credentials."
+  default     = true
+}
+
+variable "enforce_mfa" {
+  type        = bool
+  description = "Requires users to login with MFA for most AWS actions."
+  default     = false
 }
